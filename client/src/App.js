1a3ab18b1ffa4f4bd5a4c4b4ec9fc705d9c755ff
Lanfear7 <brennan1086@gmail.com>
2021-03-19 16:19:41 -0700

    Install fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Courses from './components/Courses'
 import withContext from './Context'
-import CourseDetails from './components/CourseDetails'
 import {Route, Switch, BrowserRouter, Redirect} from 'react-router-dom'
 import '../src/global.css'
+import CourseDetails from './components/CourseDetail'
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
-import UpdateCourse from './components/UpdataCourse';
+import UpdateCourse from './components/UpdateCourse';
 import UserSignOut from './components/UserSignOut';
 import CreateCourse from './components/CreateCourse';
 import Header from './components/Header'
 import PrivateRoute from './PrivateRoute'
 import notFound from './components/NotFound'
-import forbidden from './components/forbidden'
+import forbidden from './components/Forbidden'
 import UnhandledError from './components/UnhandledError'
 
 const HeaderWithContext = withContext(Header)
